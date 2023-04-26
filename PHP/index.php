e4f78a3f78965dab9f015942d28cf3cbbaa8c118
Dxvidpipet <dxvidpipet@gmail.com>
2023-04-25 23:11:28 -0500

    Herencia en PHPy Java

--- PHP/index.php
+++ PHP/index.php
@@ -1,7 +1,16 @@
 <?php
 
 require_once('car.php');
-require_once('Account.php');
+require_once('account.php');
+require_once('uberX.php');
+require_once('uberPool.php');
 
 $car = new Car("AW456", new Account("Andres Herrera", "AMS123"));
 $car->printDataCar();
+
+$uberX = new UberX("AW456", new Account("Andrea Ferran", "ANDA765"), "Chevrolet", "Spark");
+$uberX->printDataCar();
+
+$uberPool = new UberPool("RT342", new Account("Carlos Martinez", "WER432"), "Renault", "Logan");
+$uberPool->printDataCar();
+?>
