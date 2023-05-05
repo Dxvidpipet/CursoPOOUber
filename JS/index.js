ea2153ace8676fc63b7f3d6ce44e2897423e629a
Dxvidpipet <dxvidpipet@gmail.com>
2023-05-04 22:19:09 -0500

    Herencias en Account

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,7 @@
 var car = new Car("AWS456",new Account("Andres Herrera", "1312501") );
 car.passenger = 4;
 car.printDataCar();
+
+var uberX = new UberX("AWS456",new Account("Andrea Ferran", "ANDA876"), "Chevrolet", "Spark");
+uberX.passenger = 4;
+uberX.printDataCar();
